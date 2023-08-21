7faccfd217be37bd6b23ef55fc1a69163b80024d
shibadog <shiba_dog@outlook.com>
2023-08-21 07:48:25 +0000

    caを分けてみる

--- certs/create_cart.sh
+++ certs/create_cart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 自己署名ルートCA作成
-openssl req -x509 -sha256 -days 3650 -newkey rsa:4096 -keyout rootCA.key -out rootCA.crt
+openssl req -x509 -sha256 -days 3650 -newkey rsa:4096 -keyout serverCA.key -out serverCA.crt
 # パスフレーズはchangeit
 # CNはmtls.sample.shibadog.netにしてみた。
 
@@ -11,26 +11,26 @@ openssl req -new -newkey rsa:4096 -keyout localhost.key -out localhost.csr
 # CNはlocalhost
 
 # ルートCA証明書（rootCA.crt）とその 秘密鍵（rootCA.key）を使用してリクエストに署名
-openssl x509 -req -CA rootCA.crt -CAkey rootCA.key -in localhost.csr -out localhost.crt -days 365 -CAcreateserial -extfile localhost.ext
+openssl x509 -req -CA serverCA.crt -CAkey serverCA.key -in localhost.csr -out localhost.crt -days 365 -CAcreateserial -extfile localhost.ext
 
 # キーストアファイルにインポートするための p12 形式を作成する
 openssl pkcs12 -export -out localhost.p12 -name "localhost" -inkey localhost.key -in localhost.crt
 
-# トラストストアを作成する
-# パスワードはchangeit
-keytool -importcert -noprompt -alias ca -file rootCA.crt -keystore server-truststore.p12
-
 # keystoreを作成する
 keytool -importkeystore -srckeystore localhost.p12 -srcstoretype PKCS12 -destkeystore keystore.jks -deststoretype JKS
+
+# 自己署名ルートCA作成
+openssl req -x509 -sha256 -days 3650 -newkey rsa:4096 -keyout clientCA.key -out clientCA.crt
 
 # クライアント側を作る
 openssl req -new -newkey rsa:4096 -nodes -keyout client.key -out client.csr
 
 # CAで証明書に署名をする
-openssl x509 -req -CA rootCA.crt -CAkey rootCA.key -in client.csr -out client.crt -days 365 -CAcreateserial
+openssl x509 -req -CA clientCA.crt -CAkey clientCA.key -in client.csr -out client.crt -days 365 -CAcreateserial
 
 # クライアントも p12 を作る
 openssl pkcs12 -export -out client.p12 -name "client" -inkey client.key -in client.crt
 
-# CA証明書のPEMを作成してみる
-openssl pkcs12 -in server-truststore.p12 -out ca.pem -nodes
+# トラストストアを作成する
+# パスワードはchangeit
+keytool -importcert -noprompt -alias ca -file clientCA.crt -keystore server-truststore.p12
